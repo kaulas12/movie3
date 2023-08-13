66e2aee09d5450527b787f584bede1b64dc948ac
kaulas12 <kaulasdevyani12@gmail.com>
2023-08-13 15:33:55 +0530

    Second

---
+++ build/precache-manifest.6d308d01c2b151fb541be84dd5c888eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "837bc59fa1c3d7e52036",
+    "url": "/movie/static/css/main.f66e37e7.chunk.css"
+  },
+  {
+    "revision": "837bc59fa1c3d7e52036",
+    "url": "/movie/static/js/main.837bc59f.chunk.js"
+  },
+  {
+    "revision": "c60e6e391c0a934e9ab9",
+    "url": "/movie/static/js/1.c60e6e39.chunk.js"
+  },
+  {
+    "revision": "823051b9d406474d9276",
+    "url": "/movie/static/js/2.823051b9.chunk.js"
+  },
+  {
+    "revision": "fc1b6bdb03862c9c243e",
+    "url": "/movie/static/js/runtime~main.fc1b6bdb.js"
+  },
+  {
+    "revision": "cf167bb7728fe3126520db02fa939bf0",
+    "url": "/movie/index.html"
+  }
+];
---
+++ src/useFetch.js
@@ -0,0 +1,46 @@
+import  { useState, useEffect } from "react";
+
+// setting the api link
+export const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=cf4f2368`;
+
+
+const useFetch = (apiParams) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState({ show: "false", msg: "" });
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (url) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      console.log(data);
+      if (data.Response === "True") {
+        setIsLoading(false);
+        setMovie(data.Search || data);
+        setIsError({ show: "false", msg: "" });
+      } else {
+        setIsError({ show: "true", msg: data.Error });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // debouncing in react js
+  useEffect(() => {
+    let timeOut = setTimeout(() => {
+      getMovie(`${API_URL}&s=${apiParams}`);
+    }, 1000);
+    console.log("set");
+    return () => {
+      clearTimeout(timeOut);
+      console.log("clear");
+    };
+  }, [apiParams]);
+
+  return { isLoading, isError, movie };
+};
+
+export default useFetch;
