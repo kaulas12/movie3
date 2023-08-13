d1452854ac05f95b6f64ce89091c14455f86c529
kaulas12 <kaulasdevyani12@gmail.com>
2023-08-13 15:42:43 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.3adee268dc268e2cd40a3f3682556b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9baec2ebb430e39f79a4",
+    "url": "/movie3/static/css/main.9a5e643c.chunk.css"
+  },
+  {
+    "revision": "9baec2ebb430e39f79a4",
+    "url": "/movie3/static/js/main.9baec2eb.chunk.js"
+  },
+  {
+    "revision": "c60e6e391c0a934e9ab9",
+    "url": "/movie3/static/js/1.c60e6e39.chunk.js"
+  },
+  {
+    "revision": "f594fb78e2677b1a360d",
+    "url": "/movie3/static/js/2.f594fb78.chunk.js"
+  },
+  {
+    "revision": "d6692a15ab3c3680bcc8",
+    "url": "/movie3/static/js/runtime~main.d6692a15.js"
+  },
+  {
+    "revision": "0efab679873377747e04586fdf45d48e",
+    "url": "/movie3/index.html"
+  }
+];
